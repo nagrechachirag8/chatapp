96d8dd1734e18f37e226139ff83a1da039a92cf4
nagrechachirag8 <nagrechachirag8@gmail.com>
2024-03-27 19:12:47 +0530

    added where clause to query

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,8 +1,9 @@
 import React, { useState, useRef, useEffect } from "react";
 import Message from "./Message";
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { collection, onSnapshot, orderBy, query, where } from "firebase/firestore";
 import { db } from "../firebase/firebase-config";
 import SendMessage from "./SendMessage";
+import { useSelector } from "react-redux";
 
 const style = {
     main: `flex flex-col p-[10px] overflow-auto mb-[50px]`
@@ -10,10 +11,14 @@ const style = {
 
 function Chat() {
     const [messages, setMessages] = useState([]);
+    const roomname = useSelector((state) => {
+        return state.roomname.roomName
+    })
 
+    const messagesRef = collection(db, 'messages');
 
     useEffect(() => {
-        const q = query(collection(db, 'messages'), orderBy('timestamp'))
+        const q = query(messagesRef, where("room", "==", roomname) ,orderBy('timestamp'))
         const unsubscribe = onSnapshot(q, (querySnapshot) => {
             let messagesArray = [];
             querySnapshot.forEach((doc) => {
