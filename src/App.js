37f525c4f0f78202acf5a84e459a9f80ec033ded
nagrechachirag8 <nagrechachirag8@gmail.com>
2024-03-27 16:34:37 +0530

    Added chat room feature

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { auth } from "./firebase/firebase-config";
 import { useAuthState } from 'react-firebase-hooks/auth';
-import Chat from "./components/Chat";
+import ChatRoom from "./components/ChatRoom";
 
 const style = {
   appContainer: `max-w-[728px] mx-auto text-center`,
@@ -20,7 +20,8 @@ function App() {
     <div className={style.appContainer}>
       <section className={style.sectionContainer}>
         <Navbar />
-        {user ? <Chat /> : null}
+        {/* {user ? <Chat /> : null} */}
+        { user ? <ChatRoom /> : null }
       </section>
     </div>
   );
---
+++ src/components/ChatRoom.js
@@ -0,0 +1,31 @@
+import React, { useRef, useState } from "react";
+import Chat from "./Chat";
+
+const style = {
+    formContainer: `items-center justify-center flex flex-col p-20 m-2 mt-[10%]`,
+    room: `flex flex-col`,
+    label: `text-left text-l pr-0 pt-1.5 text-solid`,
+    input: `mt-1.5 mr-0 px-2.5 py-2.5 rounded-lg`,
+    button: `border border-solid border-black px-2.5 py-2.5 rounded-lg mt-2.5`
+}
+
+function ChatRoom() {
+    const [room, setRoom] = useState(null);
+    const roomInputRef = useRef(null);
+
+    return(
+        <div>
+            {
+                room ? (<Chat /> ) : (<div className={style.formContainer}>
+                                                        <div className={style.room}>
+                                                            <label className={style.label}>Enter room name</label>
+                                                            <input className={style.input} type="text" placeholder="room name" ref={roomInputRef} />
+                                                            <button className={style.button} onClick={() => setRoom(roomInputRef.current.value)}>Enter Chat</button>
+                                                        </div>
+                                                    </div>)
+            }
+        </div>
+    );
+}
+
+export default ChatRoom;
