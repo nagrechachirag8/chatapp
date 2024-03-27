f3b1f5895029fba919ed7239f4ac03c4b72ba450
nagrechachirag8 <nagrechachirag8@gmail.com>
2024-03-27 18:59:35 +0530

    added room name to Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,22 @@ import { auth } from "../firebase/firebase-config";
 import { useAuthState } from "react-firebase-hooks/auth";
 import SignIn from "./SignIn";
 import LogOut from "./LogOut";
+import { useSelector } from "react-redux";
 
 const style = {
     nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
     heading: `text-white text-3xl`
 }
 function Navbar() {
+    const roomname = useSelector((state) => {
+        return state.roomname.roomName
+    });
+
     const [user] = useAuthState(auth);
     return(
             <div className={style.nav}>
                 <h1 className={style.heading}>
-                    Chat App
+                    {roomname ? roomname : 'Chat App'}
                 </h1>
                 { user ? <LogOut /> : <SignIn /> }
             </div>
